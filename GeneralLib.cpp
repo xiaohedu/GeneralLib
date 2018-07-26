32a4f320e94a30c0518a0a4636544d3b3f719412
wangxin@zhongxiangkeji.com <wangxin@zhongxiangkeji.com>
2018-07-26 16:04:21 +0800

    CLog类初步可以利用,但内存存在泄露,待解决.

--- GeneralLib.cpp
+++ GeneralLib.cpp
@@ -1,110 +1,68 @@
-#include <iostream>
-#include <cstdlib>
-
-#include "log4cplus/logger.h"
-#include "log4cplus/configurator.h"
-#include "log4cplus/helpers/loglog.h"
-#include "log4cplus/helpers/stringhelper.h"
-#include "log4cplus/loggingmacros.h"
-
-//#include <log4cplus/logger.h>
-//#include <log4cplus/configurator.h>
-//#include <log4cplus/helpers/loglog.h>
-//#include <log4cplus/helpers/stringhelper.h>
-//#include <log4cplus/loggingmacros.h>
-
-
-#include <iomanip>//std::setprecision
-#include <stdio.h>
-#include <cstring>
-#include <string.h>
-#include <zconf.h>
-#include <sstream>
-
 #include "GeneralLib.h"
+
+
+#include <cstdlib>
+#include <log4cplus/logger.h>
+#include <log4cplus/configurator.h>
+#include <log4cplus/helpers/loglog.h>
+#include <log4cplus/helpers/stringhelper.h>
+#include <log4cplus/loggingmacros.h>
+#include <iomanip> //std::setprecision
+#include <iostream>
 
 using namespace std;
 using namespace log4cplus;
 using namespace log4cplus::helpers;
 
-int main()
-{
-    CLog m_log;
-    m_log.writeErrorLog("123");
 
-
-    return 0;
-}
+static Logger logger;
 
 CLog::CLog()
-        :m_strConfigPath("")
 {
-    //获取配置文件路径
-    char dir[PATH_MAX] = {0};
-    char syspath[PATH_MAX] = {0};
-    int n = readlink("/proc/self/exe", dir, PATH_MAX);
-
-    const char *ptr;
-    ptr = strrchr(dir, '/');
-
-    int length = strlen(dir) - strlen(ptr);
-    string s1(dir);
-    string s2;
-    s2 = s1.substr(0, length);
-    memcpy(syspath, (char*)s2.data(), PATH_MAX);
-
-    m_strConfigPath = syspath;
-    m_strConfigPath += "/log4cplus.properties";
-
-
 
     log4cplus::initialize();
+
     //log4cplus.rootLogger//
-    PropertyConfigurator::doConfigure(LOG4CPLUS_TEXT(m_strConfigPath));
+    PropertyConfigurator::doConfigure(LOG4CPLUS_TEXT("log.properties"));
+
+    logger = Logger::getRoot();
+
+#ifndef NDEBUG
+    cout<<"In CLog() constructor..."<<endl;
+#endif
 
 }
 
-CLog::CLog(string strConfigPath)
-        :m_strConfigPath(strConfigPath)
-{
-    log4cplus::initialize();
-    //log4cplus.rootLogger//
-    PropertyConfigurator::doConfigure(LOG4CPLUS_TEXT(m_strConfigPath));
-}
-
-
-//析构函数.退出时进行shutdown()操作
 CLog::~CLog()
 {
     log4cplus::Logger::shutdown();
+
+#ifndef NDEBUG
+    cout<<"In ~CLog() destructor..."<<endl;
+#endif
 }
 
-string CLog::getConfigPath()
+void CLog::WriteLog()
 {
-    return m_strConfigPath;
-}
+    //Logger logger = Logger::getRoot();
 
-void CLog::writeErrorLog(string strError)
-{
-    Logger logger = Logger::getRoot();
+    // trace
+    LOG4CPLUS_TRACE(logger, "trace and get the fingerprint: " << "random integer: " << random());
+    // debug
+    LOG4CPLUS_DEBUG(logger, "this is debug log: " << "random integer: "<< random());
+    // info
+    LOG4CPLUS_INFO(logger, "the information centry...." << "[ 1 + 1 = " << 1 + 1 << "]");
+    // warn
+    int i = 10;
+    LOG4CPLUS_WARN(logger, "Writing warning messages to log...." << i);
     // error
-    LOG4CPLUS_ERROR(logger, strError);
-
-}
-
-void CLog::writeLog(LogLevel logLevel,string& strInfo)
-{
-    Logger logger = Logger::getRoot();
-
-    switch(logLevel)
-    {
-        case TRACE:LOG4CPLUS_TRACE(logger, strInfo);break;
-        case DEBUG:LOG4CPLUS_DEBUG(logger, strInfo);break;
-        case INFO:LOG4CPLUS_INFO(logger, strInfo);break;
-        case WARN:LOG4CPLUS_WARN(logger, strInfo);break;
-        case ERROR:LOG4CPLUS_ERROR(logger, strInfo);break;
-        case FATAL:LOG4CPLUS_FATAL(logger, strInfo);break;
-        default:break;
-    }
+    LOG4CPLUS_ERROR(logger, "ooooooh, there is an error....");
+    //fatal
+    LOG4CPLUS_FATAL(logger, "oh, my god! the fatal error occur!!!!!!!!!");
+    LOG4CPLUS_DEBUG(logger, "This is a bool: " << true);
+    LOG4CPLUS_INFO(logger, "This is a char: " << 'x');
+    LOG4CPLUS_WARN(logger, "This is a int: " << 1000);
+    LOG4CPLUS_ERROR(logger, "This is a long(hex): " << std::hex << 100000000);
+    LOG4CPLUS_FATAL(logger, "This is a double: " << std::setprecision(15) << 1.2345234234);
 
 }
--- GeneralLib.h
+++ GeneralLib.h
@@ -1,55 +1,24 @@
-//
-// Created by zxkj on 18-7-24.
-//
+#ifndef __RING_LOG_H__
+#define __RING_LOG_H__
 
-#ifndef GENERALLIB_GENERALLIB_H
-#define GENERALLIB_GENERALLIB_H
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sys/types.h>//getpid, gettid
 
-#include <string>
-
-using namespace std;
-
-//================日志记录类 start==================//
-//Name:Clog
-//Note:日志记录类.基于log4cplus的日志记录工具
-//Author:liheng
-//CreateTime:2018.07.24
-//Version:V1.0
 class CLog
 {
 public:
-    //默认构造函数.意味着采用的配置文件名称为:log4cplus.properties;配置文件路径和可执行文件路径一致
     CLog();
-
-    //自定义配置文件名称和路径
-    CLog(string strConfigPath);
 
     virtual ~CLog();
 
 public:
-
-    //定义日志级别,和log4cplus中的日志级别保持一致
-    //•TRACE •DEBUG •INFO •WARN •ERROR •FATAL
-    typedef enum _LogLevel
-    {
-        TRACE = 0,
-        DEBUG,
-        INFO,
-        WARN,
-        ERROR,
-        FATAL
-    }LogLevel;
-
-private:
-    string m_strConfigPath;//log4cplus.properties配置文件的路径.完整路径
-
-public:
-    //返回配置文件路径
-    string getConfigPath();
-
-    void writeLog(LogLevel,string& strInfo);
-
-    void writeErrorLog(string strError);
+    void WriteLog();
 };
-//================日志记录类 end==================//
-#endif //GENERALLIB_GENERALLIB_H
+
+#endif
